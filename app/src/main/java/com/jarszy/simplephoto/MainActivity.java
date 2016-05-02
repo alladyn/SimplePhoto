ed820259c293ced97a349daf0943e78d2a7811ef
jarszy <jarszy@jar.com>
2016-05-02 10:35:00 +0200

    Add camera support

--- app/src/main/java/com/jarszy/simplephoto/MainActivity.java
+++ app/src/main/java/com/jarszy/simplephoto/MainActivity.java
@@ -1,6 +1,9 @@
 package com.jarszy.simplephoto;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_camera);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +40,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         photoView = (ImageView) findViewById(R.id.imageView);
+        FloatingActionButton fba = (FloatingActionButton) findViewById(R.id.fab);
+        fba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap photoCapture = (Bitmap) extras.get("data");
+            loadAnimatedBitmap(photoCapture);
+        }
+    }
+
+    private void loadAnimatedBitmap(final Bitmap photoCapture) {
+        final Animation anim_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        final Animation anim_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                photoView.setImageBitmap(photoCapture);
+                anim_in.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                photoView.startAnimation(anim_in);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        photoView.startAnimation(anim_out);
     }
 
     @Override
